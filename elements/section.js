99cd186ec636a374734a2b34822ad5e226138882
Jeffrey Yasskin <jyasskin@gmail.com>
2014-01-12 01:33:19 -0800

    Number paragraphs.

--- elements/section.js
+++ elements/section.js
@@ -31,6 +31,39 @@
                 this.title_element = title_element;
                 this.text_title = title_element.textContent;
             }
+
+            this.numberParagraphChildren();
+        },
+
+        numberParagraphChildren: function(rootElement, para_num_start) {
+            var para_num = para_num_start || 1;
+            for (var child = (rootElement || this).firstElementChild;
+                 child;
+                 child = child.nextElementSibling) {
+                if (child instanceof CxxSectionElement)
+                    return para_num;
+                else if (child instanceof HTMLParagraphElement)
+                    this.numberParagraph(para_num++, child);
+                else if (child instanceof CxxFunctionElement) {
+                    this.numberParagraph(para_num++, child);
+                    para_num = this.numberParagraphChildren(child, para_num);
+                } else if (child instanceof CxxFunctionAttributeElement)
+                    this.numberParagraph(para_num++, child);
+            }
+            return para_num;
+        },
+
+        numberParagraph: function(number, element) {
+            var id = this.id + '.' + number;
+            if (element.id) {
+                console.warn('Paragraph already has id:', element);
+                var anchor = document.createElement('a');
+                anchor.id = id;
+                element.insertBefore(anchor, element.firstChild);
+            } else {
+                element.id = id;
+            }
+            element.setAttribute('para_num', number);
         }
     })
 })();
